19c794f585f7c11cb6b776910a3079223f991336
David Davitadze <davidtadze@gmail.com>
2019-03-26 11:15:44 +0300

    fix error handling

--- 02/calculator.cpp
+++ 02/calculator.cpp
@@ -2,7 +2,7 @@
 // Created by David Davitadze on 21/03/2019.
 //
 
-#include <exception>
+#include <stdexcept>
 
 #include "calculator.hpp"
 
@@ -14,26 +14,19 @@ int Calculator::get_grammar() {
   if(expr_[cur_pos] == ' ') move_pos();
 
   // 0xDEADBEEF as indicator of incorrect number
-  if(  expr_.begin() + cur_pos != expr_.end()
-    || val == 0xDEADBEEF) throw std::runtime_error("couldn't interpret input");
+  if(  expr_.begin() + cur_pos != expr_.end()) throw std::runtime_error{"couldn't interpret input"};
 
   return val;
 }
 
 int Calculator::get_summation() {
-  // check for correct number after operand
-  // this is needed for cases like +2 (should be incorrect)
   int val_a = get_multiplication();
-  if(val_a == 0xDEADBEEF) return val_a;
 
   while(expr_[cur_pos] == '+' || expr_[cur_pos] == '-') {
     int op = expr_[cur_pos];
     move_pos();
 
-    // check for correct number after operand
-    // this is needed for cases like 4++4 or 2+ (should be incorrect)
     int val_b = get_multiplication();
-    if(val_b == 0xDEADBEEF) return val_b;
 
     if(op == '+') {
       val_a += val_b;
@@ -46,24 +39,18 @@ int Calculator::get_summation() {
 }
 
 int Calculator::get_multiplication() {
-  // check for correct number after operand
-  // this is needed for cases like /2 (should be incorrect)
   int val_a = get_number();
-  if(val_a == 0xDEADBEEF) return val_a;
 
   while(expr_[cur_pos] == '*' || expr_[cur_pos] == '/') {
     int op = expr_[cur_pos];
     move_pos();
 
-    // check for correct number after operand
-    // this is needed for cases like 4**4 or 2/ (should be incorrect)
     int val_b = get_number();
-    if(val_b == 0xDEADBEEF) return val_b;
 
     if(op == '*') {
       val_a *= val_b;
     } else {
-      if(val_b == 0) return 0xDEADBEEF;
+      if(val_b == 0) throw std::runtime_error{"division by zero"};
       val_a /= val_b;
     }
   }
@@ -72,7 +59,7 @@ int Calculator::get_multiplication() {
 }
 
 int Calculator::get_number() {
-  int val = 0xDEADBEEF;
+  int val = 0;
   bool is_number = false;
 
   // check for negative number
@@ -85,12 +72,12 @@ int Calculator::get_number() {
   while(std::isdigit(expr_[cur_pos])) {
     if(!is_number) {
       is_number = true;
-      val = 0;
     }
 
     val = val*10 + expr_[cur_pos] - '0';
     ++cur_pos;
   }
+  if(!is_number) throw std::runtime_error{"not a number"};
   if(expr_[cur_pos] == ' ') move_pos();
 
   return val * k;
